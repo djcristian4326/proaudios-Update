70457958a60cb834d447d15703145768bf2ca57a
djcristian4326 <cris_19952010@live.com>
2019-01-01 21:50:38 -0300

    correo
    correo

--- envio.php
+++ envio.php
@@ -2,22 +2,14 @@
 error_reporting(0); 
 $Nombre = $_POST['Nombre']; 
 $Email= $_POST['Email'];  
-$Asunto=$_POST['Asunto']; 
-$header = 'From: ' . $mail ;
-$header .= "X-Mailer: PHP/" . phpversion() . " \r\n"; 
-$header .= "Mime-Version: 1.0 \r\n"; 
-$header .= "Content-Type: text/plain"; 
+$Asunto=$_POST['Asunto'];
 
 $mensaje = "Este mensaje fue enviado por " . $Nombre . " \r\n"; 
 $mensaje .= "Su e-mail es: " . $Email . " \r\n"; 
-$mensaje .="Asunto".$_POST['mensaje'] . " \r\n"; 
+$mensaje .="Asunto".$_POST['Asunto'] . " \r\n"; 
 $mensaje .= "Enviado el " . date('d/m/Y', time()); 
 
-$para = 'cris_19952010@live.com; 
-$asunto = 'Mensaje de la paguian web'; 
-
-mail($para, $asunto, utf8_decode($mensaje), $header); 
-
+mail("contacto@infinitysolition.cl", "Mesaje de paguina", utf8_decode($mensaje));
 echo 'mensaje enviado correctamente'; 
 
 ?> 
