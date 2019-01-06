94264aa2c2b10d33accea5de1eb0e2b40d014c69
inenierissistemas <cesmansilaa@gmail.com>
2019-01-06 18:17:32 -0300

    ingenierisistemas

--- envio.php
+++ envio.php
@@ -1,25 +1,23 @@
-<?php
-$nombre = $_post['nombre'];
-$mail = $_post['email'];
-$empresa = $_post['mensaje'];
+<?php 
+error_reporting(0); 
+$Nombre = $_POST['Nombre']; 
+$Email= $_POST['Email'];  
+$Asunto=$_POST['Asunto']; 
+$header = 'From: ' . $mail ;
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n"; 
+$header .= "Mime-Version: 1.0 \r\n"; 
+$header .= "Content-Type: text/plain"; 
 
-$header = 'From: ' . $mail . " \r\n";
-$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
-$header .= "Mime-Version: 1.0 \r\n";
-$header .= "Content-Type: text/plain";
+$mensaje = "Este mensaje fue enviado por " . $Nombre . " \r\n"; 
+$mensaje .= "Su e-mail es: " . $Email . " \r\n"; 
+$mensaje .="Asunto".$_POST['mensaje'] . " \r\n"; 
+$mensaje .= "Enviado el " . date('d/m/Y', time()); 
 
-$mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
-$mensaje .= "Su e-mail es: " . $mail . " \r\n";
-$mensaje .= "Mensaje: " . $_post['mensaje'] . " \r\n";
-$mensaje .= "Enviado el " . date('d/m/Y', time());
+$para = 'cris_19952010@live.com; 
+$asunto = 'Mensaje de la paguian web'; 
 
-$para = 'cristian63439@gmail.com';
-$asunto = 'Mensaje de mi sitio web';
+mail($para, $asunto, utf8_decode($mensaje), $header); 
 
-if (mail($para, $asunto, utf8_decode($mensaje), $header)){
-	header("Location:index.html");
-}
+echo 'mensaje enviado correctamente'; 
 
-
-
-?>
+?> 
